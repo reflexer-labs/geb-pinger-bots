fc44fcfe848cd285bd582a7eb4dc152b7f408707
Guillaume Felley <guillaume@felley.io>
2021-01-06 15:28:34 +0100

    Reverse previous commit, wrong stuff

--- src/index.ts
+++ src/index.ts
@@ -215,8 +215,8 @@ export const livenessChecker = async () => {
   const time = Date.now()
   // List of contract to check their lastUpdateTime value and their max time tolerance in minutes
   const checks: [string, string, number, string?][] = [
-    ['eth_medianizer', env.MEDIANIZER_ETH_ADDRESS, 270],
-    ['prai_medianizer', env.MEDIANIZER_RAI_ADDRESS, 270],
+    ['eth_medianizer', env.MEDIANIZER_ETH_ADDRESS, 150],
+    ['prai_medianizer', env.MEDIANIZER_RAI_ADDRESS, 150],
     ['eth_fsm', env.FSM_ETH_ADDRESS, 270],
     ['prai_FSM', env.FSM_RAI_ADDRESS, 270],
     ['oracle_relayer', env.ORACLE_RELAYER_ADDRESS, 270, 'redemptionPriceUpdateTime'],
