fac3d525b984b274eaeaa17eaaf2b2557a517e01
Guillaume Felley <guillaume@felley.io>
2021-02-05 16:49:20 +0100

    Change liveness checker to 5h for rate setter and coin median

--- src/index.ts
+++ src/index.ts
@@ -245,10 +245,10 @@ export const livenessChecker = async () => {
   // List of contract to check their lastUpdateTime value and their max time tolerance in minutes
   const checks: [string, string, number, string?][] = [
     ['eth_medianizer', env.MEDIANIZER_ETH_ADDRESS, 80],
-    ['prai_medianizer', env.MEDIANIZER_RAI_ADDRESS, 200],
+    ['prai_medianizer', env.MEDIANIZER_RAI_ADDRESS, 320],
     ['eth_fsm', env.FSM_ETH_ADDRESS, 80],
     ['oracle_relayer', env.ORACLE_RELAYER_ADDRESS, 80, 'redemptionPriceUpdateTime'],
-    ['rate_setter', env.RATE_SETTER_ADDRESS, 200],
+    ['rate_setter', env.RATE_SETTER_ADDRESS, 320],
     [
       'stability_fee_treasury_transfer_surplus',
       env.STABILITY_FEE_TREASURY_ADDRESS,
