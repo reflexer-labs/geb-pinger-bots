343ab45dac63ef27f1bf5b9f422994a77d850f31
Guillaume Felley <guillaume@felley.io>
2021-02-23 14:24:36 +0100

    Independently update Oracle Relayer

--- src/pingers/fsm.ts
+++ src/pingers/fsm.ts
@@ -1,5 +1,5 @@
 import { BigNumber, ethers } from 'ethers'
-import { contracts, TransactionRequest } from 'geb.js'
+import { contracts, TransactionRequest, utils } from 'geb.js'
 import { notifier } from '..'
 import { Transactor } from '../chains/transactor'
 import { now } from '../utils/time'
@@ -14,7 +14,7 @@ export class CollateralFsmPinger {
     oracleRelayerAddress: string,
     private collateralType: string,
     wallet: ethers.Signer,
-    protected minUpdateInterval
+    protected minUpdateInterval: number
   ) {
     this.transactor = new Transactor(wallet)
     this.fsm = this.transactor.getGebContract(contracts.Osm, osmAddress)
@@ -25,44 +25,86 @@ export class CollateralFsmPinger {
   }
 
   public async ping() {
-    let txFsm: TransactionRequest
+    let didUpdateFsm = false
 
     // Check if it's too early to update
-    const lastUpdatedTime = await this.fsm.lastUpdateTime()
-    if (now().sub(lastUpdatedTime).lt(this.minUpdateInterval)) {
-      // To early to update but still check if there a pending transaction.
-      // If yes continue the execution that will bump the gas price.
-      if (!(await this.transactor.isAnyTransactionPending())) {
-        console.log('To early to update')
+    const fsmLastUpdatedTime = await this.fsm.lastUpdateTime()
+
+    // Update the FSM enough time has passed or if there is a transaction pending
+    // Transaction pending means that the tx from the last run got stuck in the mempool
+    if (
+      now().sub(fsmLastUpdatedTime).gte(this.minUpdateInterval) ||
+      (await this.transactor.isAnyTransactionPending())
+    ) {
+      // Simulate call
+      let txFsm: TransactionRequest
+      try {
+        txFsm = this.fsm.updateResult()
+        await this.transactor.ethCall(txFsm)
+      } catch (err) {
+        if (
+          typeof err == 'string' &&
+          (err.startsWith('OSM/not-passed') || err.startsWith('DSM/not-passed'))
+        ) {
+          console.log('FSM not yet ready to be updated')
+        } else {
+          await notifier.sendError(`Unknown error while simulating call: ${err}`)
+        }
         return
       }
+
+      // Send FSM transaction
+      let fsmHash = await this.transactor.ethSend(txFsm, true, BigNumber.from('200000'))
+      didUpdateFsm = true
+      console.log(`FSM update sent, transaction hash: ${fsmHash}`)
+    } else {
+      console.log('To early to update the FSM')
     }
 
-    // Simulate call
-    try {
-      txFsm = this.fsm.updateResult()
-      await this.transactor.ethCall(txFsm)
-    } catch (err) {
-      if (
-        typeof err == 'string' &&
-        (err.startsWith('OSM/not-passed') || err.startsWith('DSM/not-passed'))
-      ) {
-        console.log('FSM not yet ready to be updated')
-      } else {
-        await notifier.sendError(`Unknown error while simulating call: ${err}`)
-      }
-      return
+    // == Oracle Relayer ==
+
+    // Update Oracle relayer if we just updated the fsm OR
+    // if was updated too long ago
+    if (didUpdateFsm || (await this.shouldUpdateOracleRelayer())) {
+      let txRelayer = this.oracleRelayer.updateCollateralPrice(this.collateralType)
+      // Send oracle relayer transaction
+      let relayerHash = await await this.transactor.ethSend(
+        txRelayer,
+        false,
+        BigNumber.from('200000')
+      )
+      console.log(`Oracle relayer update sent, transaction hash: ${relayerHash}`)
+    } else {
+      console.log('To early to update the Oracle Relayer')
     }
+  }
 
-    // Send OSM transaction
-    let hash = await this.transactor.ethSend(txFsm, true, BigNumber.from('200000'))
-    console.log(`FSM update sent, transaction hash: ${hash}`)
+  // Check if the oracle relayer needs to be updated by looking at the last time
+  // it was updated
+  private async shouldUpdateOracleRelayer() {
+    const currentBlock = await this.transactor.getBlockNumber()
 
-    // Directly update the relayer after updating the OSM
-    let txRelayer = this.oracleRelayer.updateCollateralPrice(this.collateralType)
+    // Get the last updated events
+    // Assume a 15sec block interval
+    const scanFromBlock = currentBlock - this.minUpdateInterval / 15
+    const events = await this.transactor.getContractEvents(
+      'event UpdateCollateralPrice(bytes32 indexed collateralType, uint256 priceFeedValue, uint256 safetyPrice, uint256 liquidationPrice)',
+      this.oracleRelayer.address,
+      scanFromBlock,
+      currentBlock
+    )
+    const lastEvents = await events
+      // Remove events related to other collateral types
+      .filter((x) => ((x.args as ethers.utils.Result).collateralType as string) === utils.ETH_A)
+      // Sort events by descending time
+      .sort((a, b) => b.blockNumber - a.blockNumber)
 
-    // Send oracle relayer transaction
-    hash = await await this.transactor.ethSend(txRelayer, false, BigNumber.from('200000'))
-    console.log(`Oracle relayer update sent, transaction hash: ${hash}`)
+    // Update if they are no recent event or the latest one is more than minUpdateInterval old
+    if (lastEvents.length === 0) {
+      return true
+    } else {
+      const lastUpdateTimeOracleRelayer = (await lastEvents[0].getBlock()).timestamp
+      return now().sub(lastUpdateTimeOracleRelayer).gte(this.minUpdateInterval)
+    }
   }
 }
