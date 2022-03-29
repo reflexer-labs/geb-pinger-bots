f96bcb70e60a102a91169a7948f7b65f5d542a82
Guillaume Felley <guillaume@felley.io>
2022-03-29 11:14:37 +0200

    New bundler with withdraw function

--- src/contracts/src/PingerBundledCall.sol
+++ src/contracts/src/PingerBundledCall.sol
@@ -16,18 +16,40 @@ interface RateSetter {
     function updateRate(address feeReceiver) external;
 }
 
+interface CoinJoin {
+    function exit(address to, uint256 wad) external;
+    function systemCoin() external returns (address);
+}
+
+interface SafeEngine {
+    function coinBalance(address user) external returns (uint256);
+    function approveSAFEModification(address) external;
+}
+
+interface Erc20 {
+    function balanceOf(address user) external returns (uint256);
+    function transfer(address to, uint256 wad) external;
+}
+
 contract PingerBundledCall {
+    address owner;
     ExternallyFundedOSM public osmEthA;
     OracleRelayer public oracleRelayer;
     CoinMedianizer public coinMedianizer;
     RateSetter public rateSetter;
+    CoinJoin public coinJoin;
     bytes32 ETH_A = 0x4554482d41000000000000000000000000000000000000000000000000000000;
+
     
-    constructor(address osmEthA_, address oracleRelayer_, address coinMedianizer_, address rateSetter_) public {
+    constructor(address osmEthA_, address oracleRelayer_, address coinMedianizer_, address rateSetter_, address _owner, address _coinJoin, address _safeEngine) public {
         osmEthA = ExternallyFundedOSM(osmEthA_);
         oracleRelayer = OracleRelayer(oracleRelayer_);
         rateSetter = RateSetter(rateSetter_);
         coinMedianizer = CoinMedianizer(coinMedianizer_);
+        owner = _owner;
+        coinJoin = CoinJoin(_coinJoin);
+
+        SafeEngine(_safeEngine).approveSAFEModification(_coinJoin);
     }
 
     function updateOsmAndEthAOracleRelayer() external {
@@ -44,5 +66,13 @@ contract PingerBundledCall {
         coinMedianizer.updateResult(feeReceiver);
         rateSetter.updateRate(feeReceiver);
     }
+
+    function withdrawPayout(address to, uint256 wad) external {
+        require(msg.sender == owner, "Not owner");
+
+        coinJoin.exit(address(this), wad);
+        Erc20(coinJoin.systemCoin()).transfer(to, wad);
+    }
+
 
 }
--- src/contracts/src/PingerBundledCall.t.sol
+++ src/contracts/src/PingerBundledCall.t.sol
@@ -6,7 +6,7 @@ import "./PingerBundledCall.sol";
 
 interface Hevm {
     function warp(uint256) external;
-}
+}   
 
 contract PingerBundledCallTest is DSTest {
     Hevm hevm;
@@ -16,8 +16,11 @@ contract PingerBundledCallTest is DSTest {
         bundler = new PingerBundledCall(
             0xD4A0E3EC2A937E7CCa4A192756a8439A8BF4bA91,
             0x4ed9C0dCa0479bC64d8f4EB3007126D5791f7851,
-            0xFbF4849a06F6e6F53EcB31D2f8BD61aA7874b268,
-            0x7Acfc14dBF2decD1c9213Db32AE7784626daEb48
+            0x92dC9b16be52De059279916c1eF810877f85F960,
+            0x7Acfc14dBF2decD1c9213Db32AE7784626daEb48,
+            address(this),
+            0x0A5653CCa4DB1B6E265F47CAf6969e64f1CFdC45,
+            0xCC88a9d330da1133Df3A7bD823B95e52511A6962
         );
 
         hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
@@ -26,6 +29,8 @@ contract PingerBundledCallTest is DSTest {
 
     function test_osm_and_oracle_relayer() public {
         bundler.updateOsmAndEthAOracleRelayer();
+
+        bundler.withdrawPayout(address(this), SafeEngine(0xCC88a9d330da1133Df3A7bD823B95e52511A6962).coinBalance(address(bundler)) / 1e27);
     }
 
     function test_coin_meidan_and_rate_setter() public {
